1f292a91d7536aeee6b66e57c43c1c18f28813a4
Joerg Wirsig <joerg.wirsig@raysono.com>
2013-01-10 11:45:04 +0100

    Erweiterung des MapHelper

--- src/main/java/de/wwservices/util/MapHelper.java
+++ src/main/java/de/wwservices/util/MapHelper.java
@@ -7,18 +7,19 @@ import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * Helper Class for common Operations on Maps. 
+ * Helper Class for common Operations on Maps.
+ * 
  * @author joergw
- *
+ * 
  */
 public class MapHelper {
 
 	/**
-	 * Creates a Set of alle Entries of the given Map Sorted by the Value.
-	 * Note that null as Keys are not supported!
+	 * Creates a set of all entries of the given map sorted by the value. 
+	 * Note that on same values the keys are compared additionally.
 	 * 
-	 * @param incomingMap
-	 * @return
+	 * @param incomingMap the map to sort
+	 * @return Set conatining sorted entries
 	 */
 	public static <K extends Comparable<K>, V extends Comparable<V>> Set<Entry<K, V>> sortedEntrySetByValue(
 			Map<K, V> incomingMap) {
@@ -35,11 +36,11 @@ public class MapHelper {
 	 * Comparator to compare to entries of a Map by their values if existent.
 	 * 
 	 * @author joergw
-	 *
+	 * 
 	 * @param <K>
 	 * @param <V>
 	 */
-	static class EntrySetComparator<K extends Comparable<K>, V extends Comparable<V>>
+	private static class EntrySetComparator<K extends Comparable<K>, V extends Comparable<V>>
 			implements Comparator<Entry<K, V>> {
 
 		@Override
@@ -60,7 +61,63 @@ public class MapHelper {
 			}
 
 		}
+	}
+
+	/**
+	 * Creates a set of all entries of the given map sorted by the value. 
+	 * Note that on same values the keys are compared additionally.
+	 * 
+	 * @param incomingMap the map to sort.
+	 * @param comparator the Comparator to compare the values
+	 * @return sorted Set.
+	 */
+	public static <K extends Comparable<K>, V> Set<Entry<K, V>> sortedEntrySetByValue(
+			Map<K, V> incomingMap, Comparator<V> comparator) {
+
+		Comparator<Entry<K, V>> entrySetComparator = new EntrySetComparator2<K, V>(
+				comparator);
+		Set<Entry<K, V>> sortedSet = new TreeSet<Entry<K, V>>(
+				entrySetComparator);
+		sortedSet.addAll(incomingMap.entrySet());
+		return sortedSet;
 
 	}
 
+	/**
+	 * Comparator to compare to entries of a Map by their values if existent.
+	 * 
+	 * @author joergw
+	 * 
+	 * @param <K>
+	 * @param <V>
+	 */
+	private static class EntrySetComparator2<K extends Comparable<K>, V>
+			implements Comparator<Entry<K, V>> {
+
+		private Comparator<V> comp;
+
+		EntrySetComparator2(Comparator<V> comparator) {
+			comp = comparator;
+		}
+
+		@Override
+		public int compare(Entry<K, V> o1, Entry<K, V> o2) {
+			if (o1.getValue() != null && o2.getValue() != null) {
+				int compareTo = comp.compare(o1.getValue(), o2.getValue());
+				if (compareTo == 0) {
+					return o1.getKey().compareTo(o2.getKey());
+				} else {
+					return compareTo;
+				}
+			} else if (o1.getValue() == null && o2.getValue() != null) {
+				return 1;
+			} else if (o1.getValue() != null && o2.getValue() == null) {
+				return -1;
+			} else {
+				return o1.getKey().compareTo(o2.getKey());
+			}
+
+		}
+	}
+	
 }
--- src/test/java/de/wwservices/util/MapHelperTest.java
+++ src/test/java/de/wwservices/util/MapHelperTest.java
@@ -1,13 +1,15 @@
 package de.wwservices.util;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.TreeMap;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.nullValue;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -39,6 +41,50 @@ public class MapHelperTest {
 		Assert.assertThat(iterator.next().getValue(), nullValue());
 		Assert.assertThat(iterator.next().getValue(), nullValue());
 
+	}
+
+	@Test
+	public void testSortComparator() {
+		Map<Integer, TestSort> mapping = new TreeMap<Integer, MapHelperTest.TestSort>();
+		mapping.put(Integer.valueOf(1), new TestSort(Integer.valueOf(10)));
+		mapping.put(Integer.valueOf(2), new TestSort(Integer.valueOf(5)));
+		mapping.put(Integer.valueOf(3), new TestSort(Integer.valueOf(8)));
+		mapping.put(Integer.valueOf(4), new TestSort(Integer.valueOf(9)));
+		Set<Entry<Integer, TestSort>> entrySet = mapping.entrySet();
+		Iterator<Entry<Integer, TestSort>> iterator = entrySet.iterator();
+		Assert.assertThat(iterator.next().getValue().getSortingnumber(), equalTo(Integer.valueOf(10)));
+		Assert.assertThat(iterator.next().getValue().getSortingnumber(), equalTo(Integer.valueOf(5)));
+		Assert.assertThat(iterator.next().getValue().getSortingnumber(), equalTo(Integer.valueOf(8)));
+		Assert.assertThat(iterator.next().getValue().getSortingnumber(), equalTo(Integer.valueOf(9)));
+		
+		entrySet = MapHelper.sortedEntrySetByValue(mapping, new TestComparator());
+		iterator = entrySet.iterator();
+		Assert.assertThat(iterator.next().getValue().getSortingnumber(), equalTo(Integer.valueOf(5)));
+		Assert.assertThat(iterator.next().getValue().getSortingnumber(), equalTo(Integer.valueOf(8)));
+		Assert.assertThat(iterator.next().getValue().getSortingnumber(), equalTo(Integer.valueOf(9)));
+		Assert.assertThat(iterator.next().getValue().getSortingnumber(), equalTo(Integer.valueOf(10)));
+	}
+
+	private class TestSort {
+		private Integer sortingnumber = Integer.valueOf(-1);
+
+		TestSort(Integer sorting) {
+			sortingnumber = sorting;
+		}
+		
+		public Integer getSortingnumber() {
+			return sortingnumber;
+		}
+	}
+	
+	private class TestComparator implements java.util.Comparator<TestSort>
+	{
+
+		@Override
+		public int compare(TestSort o1, TestSort o2) {
+			return o1.sortingnumber.compareTo(o2.sortingnumber);
+		}
+		
 	}
 
 }
